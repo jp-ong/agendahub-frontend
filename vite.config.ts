878af4b59597f195db92412cb9c91a259ab47871
John Paul Ong <jpong5202@gmail.com>
2024-08-26 13:32:01 +0800

    Add breakpoint automatic import

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+// @ts-ignore
+import path from 'node:path';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: { port: 3000 },
+  resolve: {
+    alias: {
+      // @ts-ignore
+      '@scss': path.resolve(__dirname, 'src/scss'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: "@import '@scss/config/breakpoints';\n",
+      },
+    },
+  },
 });
