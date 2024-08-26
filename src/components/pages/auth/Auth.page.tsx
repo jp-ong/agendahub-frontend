75870a9ab2564d28040f94aa55361eeee31d8022
John Paul Ong <jpong5202@gmail.com>
2024-08-26 18:11:34 +0800

    Update theme

--- src/components/pages/auth/Auth.page.tsx
+++ src/components/pages/auth/Auth.page.tsx
@@ -1,19 +1,22 @@
 import styles from './Auth.module.scss';
 import { Button } from '@components/button';
 import { InputField } from '@components/input-field';
+import { Logo } from '@components/logo';
 
 export const AuthPage: React.FC = () => {
   return (
     <div className={styles.root}>
-      <div className={styles.form}>
+      <div className={styles.container}>
+        <Logo maxWidth height={24} />
         <form>
-          <InputField maxWidth label="Username" />
-          <InputField maxWidth label="Password" />
-          <div className={styles.actions}>
-            <Button label="Sign in" variant="contained" maxWidth />
-            <Button label="Create an account" variant="outlined" maxWidth />
-          </div>
+          <InputField label="Username" />
+          <InputField label="Password" />
+          <Button label="Sign in" variant="contained" />
         </form>
+        <div className={styles.cta}>
+          <small>Don't have an account yet?</small>
+          <Button label="Create an account" variant="outlined" />
+        </div>
       </div>
     </div>
   );
